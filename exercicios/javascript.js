2d0d981b70718c345410c62144b12778f3414574
Marcelo1981 <1981cello@gmail.com>
2014-03-21 20:22:54 -0300

    javascript

---
+++ exercicios/javascript.js
@@ -0,0 +1,45 @@
+//
+//autor:marcelo santos
+//
+//funçao desse js calculadora
+//
+
+//leia valorA
+
+//escolha operação
+
+	//soma=valorA mais valorB
+		//recebe ValorA
+		//recebe valorB
+		//processa soma
+		//guarda resultado
+
+	//subtração=compara se tem valor negativo e diminua primeiro
+		//recebe valorA
+		//recebe valorB
+		//processa subtracao
+		//guarda resultado
+
+	//multiplicação= valorA vezes ValorB
+		//recebe valorA
+		//recebe valorB
+		//processa multiplicar
+		//guarda resultado	
+
+	//divisão= valorA divide ValorB
+		//recebe valorA
+		//recebe valorB
+		//processa divide
+		//guarda resultado
+
+
+//leia valorB
+
+//mostrar resultado
+
+	//pegue valorA, mais operação, mais valorB
+
+
+//mostre resultado
+
+alert( -5 + 4);
