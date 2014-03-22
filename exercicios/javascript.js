d9a89451d47e53abc4c44d2327be5bc5e07bc554
Marcelo1981 <1981cello@gmail.com>
2014-03-21 22:02:15 -0300

    calculadora funcioonando

--- exercicios/javascript.js
+++ exercicios/javascript.js
@@ -46,21 +46,51 @@ function calcular(){
 			break;
 
 		default:
-			alert("operação errada");
+			resultado ="operação errada";
 			break
 	}
 }
 
+function validar(){
+	
+	
+
+	//verificar operador e dividir e o valor é 0
+	//verificar se o primeiro operacao digitado
+}
+
+
+function verificarValorA(){
+	//verificar se os valores sao numeros
+	if (!!isNaN(valorA)) {
+		valorA = Number(prompt("digite um numero A"));
+		verificarValorA();
+	}
+}
+
+
+function verificarValorB(){
+	//verificar se os valores sao numeros
+	if (!!isNaN(valorB)) {
+		valorB = Number(prompt("digite um numero B"));
+		verificarValorB();
+	}
+}
+
 //leia valorA
-valorA = Number(prompt("digite o primeiro numero"));
+//valorA = Number(prompt("digite o primeiro numero"));
+verificarValorA();
 
 //escolha operação
 operacao = prompt("voce deseja (+)(-)(/)(*)");
 
+
 //leia valorB
-valorB = Number(prompt("digite o segundo numero"));
+//valorB = Number(prompt("digite o segundo numero"));
+verificarValorB();
 
 //executar funçao do resultado
+
 calcular();
 
 alert(resultado);
